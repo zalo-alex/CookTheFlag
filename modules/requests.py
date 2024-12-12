ca717bacaef204491e3183b2cc70d1d7c1b8d75a
Alex ZALO <zalo.alex56@gmail.com>
2024-12-12 18:44:41 +0100

    Add headers to Requests module

--- modules/requests.py
+++ modules/requests.py
@@ -2,6 +2,7 @@ from src.module import Module
 from src.layout.input import Input
 from src.layout.submit import Submit
 from src.layout.select import Select
+from src.layout.key_value import KeyValue
 
 import requests
 
@@ -11,12 +12,13 @@ class CustomModule(Module):
     layout = [
         Input("URL", "url-input", regex=r"^https?:\/\/([A-Za-z0-9.-]+)(:[0-9]{1,5})?(\/[^\s]*)?$"),
         Select("Method", "method", ["GET", "POST"]),
+        KeyValue("Headers", "headers"),
         Submit("Send", "send"),
         Input("Text Response", "output", textarea=True),
     ]
     
     def submit(type, data):
-        r = requests.request(data["method"], data["url-input"])
+        r = requests.request(data["method"], data["url-input"], headers=data["headers"])
         return {
             "output": r.text
         }
