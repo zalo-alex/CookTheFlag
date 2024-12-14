a3a73850a000cdcf763fffacf818ba6b7d86ce84
Alex Zalo <zalo.alex56@gmail.com>
2024-12-14 17:30:12 +0100

    Add NMap module

---
+++ modules/nmap.py
@@ -0,0 +1,26 @@
+from src.module import Module
+from src.layout import *
+from src.regexs import Regexs
+from src.exec import Exec
+
+class CustomModule(Module):
+    name = "NMap"
+    category = "tool"
+    layout = [
+        Input("Target", "input", regex=Regexs.IPV4),
+        CheckBox("Service version", "version"),
+        CheckBox("OS detection", "os"),
+        Submit("Scan", "scan"),
+        Input("Command", "command"),
+        Input("Result", "output", textarea=True),
+    ]
+    
+    def submit(type, data):
+        args = []
+        if data.get('version'): args.append('-sV')
+        if data.get('os'): args.append('-O')
+        cmd = ["nmap", *args, data['input']]
+        return {
+            "command": " ".join(cmd),
+            "output": Exec(cmd).run()
+        }
