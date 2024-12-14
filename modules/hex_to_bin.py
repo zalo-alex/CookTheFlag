94b0c2167d5b8bcd550d4deaf267207f619b0258
Alex Zalo <zalo.alex56@gmail.com>
2024-12-14 15:13:50 +0100

    Add Hex to Binary module

---
+++ modules/hex_to_bin.py
@@ -0,0 +1,18 @@
+from src.module import Module
+from src.layout import *
+from src.regexs import Regexs
+from src.parsers import HexParser
+
+class CustomModule(Module):
+    name = "Hex to Binary"
+    category = "encoding"
+    layout = [
+        Input("Hex Input", "input", textarea=True, regex=Regexs.HEX, parser=HexParser()),
+        Submit("Submit", "encode"),
+        Input("Binary Output", "output", textarea=True),
+    ]
+    script = """
+        return {
+            "output": parseInt(data["input"], 16).toString(2)
+        }
+    """
