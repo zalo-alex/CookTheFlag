4e4c1584b88ac8952bb96c6b9e7d408f72d4931d
Alex Zalo <zalo.alex56@gmail.com>
2024-12-14 17:30:03 +0100

    Update Exec

--- src/exec.py
+++ src/exec.py
@@ -2,9 +2,8 @@ import subprocess
 
 class Exec:
     
-    def __init__(self, process, args):
-        self.process = process
+    def __init__(self, args):
         self.args = args
     
     def run(self):
-        return subprocess.run([self.process, *self.args], capture_output=True, text=True).stdout
+        return subprocess.run([*self.args], capture_output=True, text=True).stdout
