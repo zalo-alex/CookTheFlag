c2a9383f3e2fa1f8c35cedb2caadcea424d77846
Alex ZALO <zalo.alex56@gmail.com>
2024-12-17 18:40:03 +0100

    Add files managment system

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+import os
 import traceback
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sock import Sock
 from types import GeneratorType
 
 from src.module_manage import ModuleManage
+from src.files import Files
 
 import base64
 import json
@@ -12,6 +15,7 @@ app = Flask(__name__)
 sock = Sock(app)
 
 manager = ModuleManage()
+files = Files()
 
 @app.errorhandler(404) 
 def not_found(e): 
@@ -20,6 +24,43 @@ def not_found(e):
 @app.route("/")
 def index():
     return render_template("index.html", modules=manager.modules, categories=manager.categories)
+
+@app.route("/files")
+def files_view():
+    path = files.get_path(request.args.get("path", ".")) 
+
+    if not files.exists(path):
+        return render_template("files.html", path=path, folders=[], files=[], downloadables=[], modules=manager.modules, categories=manager.categories, exists=False)
+    
+    folders, _files = files.list_dir(path)
+    # downloadables = files.get_downloadables(path) TODO: Implement downloadables feature
+    
+    return render_template("files.html", path=path, folders=folders, files=_files, downloadables=[], modules=manager.modules, categories=manager.categories, exists=True)
+
+@app.route("/files/upload", methods=["POST"])
+def files_upload():    
+    path = files.get_path(request.args.get("path", "."))
+
+    if not files.exists(path):
+        return redirect(f"/files?path={request.args.get('path', '.')}")
+    
+    _files = request.files.getlist("files")
+    
+    for file in _files:
+        files.save_file(path, file)
+
+    return redirect(f"/files?path={request.args.get('path', '.')}")
+
+@app.route("/files/delete", methods=["POST"])
+def files_delete():    
+    path = files.get_path(request.args.get("path", "."))
+
+    if not files.exists(path):
+        return redirect(f"/files?path={request.args.get('path', '.')}")
+    
+    files.delete_file(path)
+
+    return redirect(f"/files?path={os.path.dirname(path)}")
 
 @app.route("/regex/<b64_query>")
 def regex(b64_query):
---
+++ src/files.py
@@ -0,0 +1,76 @@
+import os
+import shutil
+from pathlib import Path
+
+class DownloadableFile:
+    
+    def __init__(self, url):
+        self.url = url
+
+class Files:
+
+    structure = {
+        "wordlists": {
+            "10-million-password-list-top-100000.txt": DownloadableFile("https://raw.githubusercontent.com/danielmiessler/SecLists/refs/heads/master/Passwords/Common-Credentials/10-million-password-list-top-100000.txt")
+        }
+    }
+
+    def __init__(self):
+        self.data_dir = "/data" if os.getenv("DOCKER") else os.path.join(Path.home(), ".cooktheflag")
+        self.init_folders(self.data_dir, self.structure)
+
+    def get_absolute_path(self, path):
+        return os.path.join(self.data_dir, self.get_path(path))
+    
+    def get_path(self, path):
+        return os.path.relpath(os.path.normpath(os.path.join("/", path)), "/")
+
+    def init_folders(self, path, sub):
+        for folder in sub:
+            if type(sub[folder]) == dict:
+                folder_path = os.path.join(path, folder)
+                print(folder_path)
+                os.makedirs(folder_path, exist_ok=True)
+                self.init_folders(folder_path, sub[folder])
+    
+    def exists(self, path):
+        return os.path.exists(self.get_absolute_path(path))
+
+    def structure_from_path(self, path):
+        parts = path.split("/")
+        sub = self.structure
+        for part in parts:
+            if part not in sub:
+                return {}
+            sub = sub[part]
+        return sub
+
+    def get_downloadables(self, path):
+        downloadables = []
+        strct = self.structure_from_path(path)
+        for file in strct:
+            if type(strct[file]) == DownloadableFile:
+                downloadables.append(file)
+        return downloadables
+
+    def list_dir(self, path):
+        abs_path = self.get_absolute_path(path)
+        folders = []
+        files = []
+        for file in os.listdir(abs_path):
+            p = os.path.join(abs_path, file)
+            if os.path.isdir(p):
+                folders.append(file)
+            else:
+                files.append(file)
+        return folders, files
+
+    def save_file(self, path, file):
+        file.save(os.path.join(self.get_absolute_path(path), file.filename))
+    
+    def delete_file(self, path):
+        path = self.get_absolute_path(path)
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(path)
