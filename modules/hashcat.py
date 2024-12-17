8ef182e9556a19c4104f176fdb66e393b1ab67f6
Alex Zalo <zalo.alex56@gmail.com>
2024-12-17 19:27:40 +0100

    Add Hashcat module

---
+++ modules/hashcat.py
@@ -0,0 +1,35 @@
+from src.module import Module
+from src.layout import *
+from src.regexs import Regexs
+from src.exec import Exec
+
+class CustomModule(Module):
+    name = "Hashcat"
+    category = "tool"
+    layout = [
+        Input("Hash", "input"),
+        Select("Mode", "mode", {
+            "MD5": "0",
+            "SHA1": "100",
+        }),
+        Input("Wordlist", "wordlist"),
+        Submit("Crack", "crack"),
+        Input("Command", "command"),
+        Input("Result", "result"),
+        Input("Output", "output", textarea=True)
+    ]
+    
+    def submit(type, data):
+        open("hash.txt", "w").write(data["input"])
+        cmd = ["hashcat", "-m", data["mode"], "hash.txt", data["wordlist"]]
+        yield {
+            "command": " ".join(cmd)
+        }
+        for line in Exec(cmd).run():
+            yield {
+                "output": line
+            }
+        for line in Exec(cmd + ["--show"]).run():
+            yield {
+                "result": line
+            }
